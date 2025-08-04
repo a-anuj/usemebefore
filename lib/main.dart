a17aafc046e5b6e8c35ea3a826f234560cbed7dd
a-anuj <anujamirthalingam5@gmail.com>
2025-08-04 23:58:19 +0530

    modified the main.dart file to integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:usemebefore/widgets/Auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:usemebefore/widgets/Landing.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF00A8CC))
       ),
-      home : AuthScreen(),
+        home: StreamBuilder(stream:
+        FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx,snapshot){
+              if(snapshot.hasData){
+                return Landing();
+              }
+              return AuthScreen();
+            }),
   ) );
 }
 
