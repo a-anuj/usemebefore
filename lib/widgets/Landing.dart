ac6012274a40869a36b379ba89493f65d91f45ec
a-anuj <anujamirthalingam5@gmail.com>
2025-08-05 08:28:20 +0530

    add an appbar to the landing page

--- lib/widgets/Landing.dart
+++ lib/widgets/Landing.dart
@@ -1,32 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
-class Landing extends StatelessWidget{
-  const Landing({super.key});
-  
+class Landing extends StatefulWidget{
+  const Landing ({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LandingState();
+  }
+}
+
+class _LandingState extends State<Landing>{
   @override
   Widget build(BuildContext context) {
-    return(
-        Scaffold(
-      body: Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children : [
-
-            Text("Welcome to the landing page!"),
-            ElevatedButton(
-                onPressed: (){
-                  FirebaseAuth.instance.signOut();
-                },
-                child: Text("Logout")
-            )
-          ],
-        )
-
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        title:Text(
+            "useMeBefore",
+            style: GoogleFonts.lato(
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.onSecondaryContainer,
+              ),
+            ),
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        actions: [
+          IconButton(
+              onPressed: (){
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                  Icons.logout_outlined,
+                color: Theme.of(context).colorScheme.onSecondaryContainer,
+              ),
+          )
+        ],
       ),
-    )
+      body: Center(
+        child: Text(
+            "Added items will be listed here...",
+            style:GoogleFonts.poppins(
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+              color: Colors.grey,
+            ) ,
+        ),
+      ),
     );
+
   }
 }
