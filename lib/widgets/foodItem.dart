a7bf7657f3c59e3799333f87ec153a9e27061d2a
a-anuj <anujamirthalingam5@gmail.com>
2025-08-05 08:50:02 +0530

    create a ItemClass  to store the structure of the item entered

---
+++ lib/widgets/foodItem.dart
@@ -0,0 +1,35 @@
+class FoodItem {
+  final String title;
+  final String expiryDate;
+  final String storage;
+  final String note;
+  final String imageUrl;
+
+  FoodItem({
+    required this.title,
+    required this.expiryDate,
+    required this.storage,
+    required this.note,
+    required this.imageUrl,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'expiryDate': expiryDate,
+      'storage': storage,
+      'note': note,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  factory FoodItem.fromJson(Map<String, dynamic> json) {
+    return FoodItem(
+      title: json['title'],
+      expiryDate: json['expiryDate'],
+      storage: json['storage'],
+      note: json['note'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
