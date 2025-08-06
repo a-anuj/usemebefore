d8d312ccb7cf7c1665f3e99fa8ff2dd27b00968b
a-anuj <anujamirthalingam5@gmail.com>
2025-08-06 09:28:55 +0530

    revoked the plan of adding notifications for now

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:usemebefore/widgets/Landing.dart';
 
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
